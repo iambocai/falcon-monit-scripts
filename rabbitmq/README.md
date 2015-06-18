c2219f69772844654f290c893c7d713270d61f8b
程生波 <chengshengbo@lubanec.com>
2015-06-18 10:19:00 +0800

    添加rabbitmq监控脚本
    添加rabbitmq监控脚本及说明

---
+++ rabbitmq/README.md
@@ -0,0 +1,33 @@
+open-falcon rabbitmq监控脚本
+================================
+
+系统需求
+--------------------------------
+操作系统：Linux
+Python >= 2.6
+python-simplejson
+
+主要逻辑
+--------------------------------
+从rabbitmq-server的api接口读取相关数据，然后推送到falco-agent
+
+汇报字段
+--------------------------------
+| key |  tag | type | note |
+|-----|------|------|------|
+|rabbitmq.messages_ready|name(Queue名字)|GAUGE|队列中处于等待被消费状态消息数|
+|rabbitmq.messages_unacknowledged|name(Queue名字)|GAUGE|队列中处于消费中状态的消息数|
+rabbitmq.messages_total|name(Queue名字)|GAUGE|队列中所有未完成消费的消息数，等于messages_ready+messages_unacknowledged|
+rabbitmq.ack_rate|name(Queue名字)|GAUGE|消费者ack的速率|
+rabbitmq.deliver_rate|name(Queue名字)|GAUGE|deliver的速率|
+rabbitmq.deliver_get_rate|name(Queue名字)|GAUGE|deliver_get的速率|
+rabbitmq.publish_rate|name(Queue名字)|GAUGE|publish的速率|
+
+
+使用方法
+--------------------------------
+1. 根据实际部署情况，修改15,16行的rabbitmq-server管理端口和登录用户名密码
+2. 将脚本加入crontab即可
+
+
+授权类型：MIT
---
+++ rabbitmq/rabbitmq-monitor.py
@@ -0,0 +1,84 @@
+#!/bin/env python
+#-*- coding:utf-8 -*-
+
+__author__ = 'iambocai'
+
+import sys, urllib2, base64, json, time,socket
+
+
+step = 60
+ip = socket.gethostname()
+ts = int(time.time())
+keys = ('messages_ready', 'messages_unacknowledged')
+rates = ('ack', 'deliver', 'deliver_get', 'publish')
+
+request = urllib2.Request("http://%s:15672/api/queues" %ip)
+base64string = base64.encodestring('guest:guest').replace('\n', '')
+request.add_header("Authorization", "Basic %s" % base64string)   
+result = urllib2.urlopen(request)
+data = json.loads(result.read())
+tag = ''
+#tag = sys.argv[1].replace('_',',').replace('.','=')
+
+p = []
+for queue in data:
+	# ready and unack
+	msg_total = 0
+	for key in keys:
+		q = {}
+		q["endpoint"] = ip
+		q['timestamp'] = ts
+		q['step'] = step
+		q['counterType'] = "GAUGE"
+		q['metric'] = 'rabbitmq.%s' % key
+		q['tags'] = 'name=%s,%s' % (queue['name'],tag)
+		q['value'] = int(queue[key])
+		msg_total += q['value']
+		p.append(q)
+
+	# total
+	q = {}
+	q["endpoint"] = ip
+	q['timestamp'] = ts
+	q['step'] = step
+	q['counterType'] = "GAUGE"
+	q['metric'] = 'rabbitmq.messages_total'
+	q['tags'] = 'name=%s,%s' % (queue['name'],tag)
+	q['value'] = msg_total
+	p.append(q)
+	
+	# rates
+	for rate in rates:
+		q = {}
+		q["endpoint"] = ip
+		q['timestamp'] = ts
+		q['step'] = step
+		q['counterType'] = "GAUGE"
+		q['metric'] = 'rabbitmq.%s_rate' % rate
+		q['tags'] = 'name=%s,%s' % (queue['name'],tag)
+		try:
+			q['value'] = int(queue['message_stats']["%s_details" % rate]['rate'])
+		except:
+			q['value'] = 0
+		p.append(q)
+
+print json.dumps(p, indent=4)
+
+
+method = "POST"
+handler = urllib2.HTTPHandler()
+opener = urllib2.build_opener(handler)
+url = 'http://127.0.0.1:1988/v1/push'
+request = urllib2.Request(url, data=json.dumps(p) )
+request.add_header("Content-Type",'application/json')
+request.get_method = lambda: method
+try:
+    connection = opener.open(request)
+except urllib2.HTTPError,e:
+    connection = e
+
+# check. Substitute with appropriate HTTP code.
+if connection.code == 200:
+    print connection.read()
+else:
+    print '{"err":1,"msg":"%s"}' % connection
